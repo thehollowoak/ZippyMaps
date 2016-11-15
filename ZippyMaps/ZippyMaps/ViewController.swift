1f5c426869bb39488f5b6b6f12e528f2712030ab
Maxwell Kay <mak138@254-13.cs.uakron.edu>
2016-11-15 14:28:26 -0500

    Created mapView and set default properties and location.

--- ZippyMaps/ZippyMaps/ViewController.swift
+++ ZippyMaps/ZippyMaps/ViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    //UA's GPS coords 41.075931, -81.511134
+    @IBOutlet weak var AkronMap: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let defaultLocation = CLLocationCoordinate2D(latitude: 41.075931, longitude: -81.511134)
+        //Set the map to the hybrid display
+        AkronMap.mapType = MKMapType.hybrid
+        
+        //Area in meters
+        let areaRegion: CLLocationDistance = 500
+        let defaultRegion = MKCoordinateRegionMakeWithDistance(defaultLocation, areaRegion, areaRegion)
+        AkronMap.setRegion(defaultRegion, animated: false)
+        print("HERE")
+        print(AkronMap.region)
+        AkronMap.mapType = MKMapType.hybrid
+        
     }
 
     override func didReceiveMemoryWarning() {
