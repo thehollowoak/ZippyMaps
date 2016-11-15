b784fc1f873dbbfbe1683e86fd18b03c80255005
Maxwell Kay <mak138@254-13.cs.uakron.edu>
2016-11-15 13:55:29 -0500

    Initial setup

---
+++ ZippyMaps/ZippyMaps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZippyMaps
+//
+//  Created by Kay,Maxwell on 11/15/16.
+//  Copyright © 2016 Kay,Maxwell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
