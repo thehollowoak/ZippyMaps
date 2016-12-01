4ad675b16b1663c64453ce205581c8d839367cec
Maxwell Kay <mak138@254-13.cs.uakron.edu>
2016-12-01 15:14:02 -0500

    Began work on class selection table.

---
+++ ZippyMaps/ZippyMaps/ClassScheduleViewCellTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ClassScheduleViewCellTableViewCell.swift
+//  ZippyMaps
+//
+//  Created by Kay,Maxwell on 12/1/16.
+//  Copyright © 2016 Kay,Maxwell. All rights reserved.
+//
+
+import UIKit
+
+class ClassScheduleViewCellTableViewCell: UITableViewCell {
+    @IBOutlet weak var classNameLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    
+
+}
